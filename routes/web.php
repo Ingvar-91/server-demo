a0ef8dd1b3305676e0368b39cefc741947edbe32
igor <ingvar-91@inbox.ru>
2018-07-29 15:27:58 +0600

    FIX

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,12 @@
 <?php
 
-/*Route::get('/image', function(){
-    $originalWidth = 3840;
-    $originalHeight = 2160;
-    $maxWidth = 382;
-    $maxHeight = 215;
-    $ratio = $originalHeight / $maxHeight;
-    $x1 = ceil(167 * $ratio);
-    $x2 = ceil(382 * $ratio);
-    $y1 = ceil(0 * $ratio);
-    $y2 = ceil(215 * $ratio);
-    $w = $y2 - $y1;
-    $h = $y2 - $y1;
-    $img = Image::make('1.jpg')->heighten($originalHeight)->crop($w, $h, $x1, $y1);
-    return $img->response('jpg');
-});*/
-
+/*
 Route::group([
     'namespace'  => 'Api\v1'
 ], function (){
     
     Route::get('/{id}', ['uses' => 'Admin\UsersController@getById']);
 });
+*/
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
- */
+Route::get('/', ['uses' => 'Site\HomeController@index']);
